b89f518b00409c8cdabbc67e2152fd75e9788cc0
Boris Fackovec <borislavujo@gmail.com>
2015-04-24 18:28:49 +0100

    Update potentialAMB.m

--- ala-dip/potentialAMB.m
+++ ala-dip/potentialAMB.m
@@ -3,9 +3,7 @@ subor = fopen('start', 'w');
 for i=1:size(X,1)
 	fprintf(subor,"%20.9f%20.9f%20.9f\n",X(i,1),X(i,2),X(i,3));
 endfor
-system("cp start backup_pociatek");
 fclose(subor)
-%save -ascii start X ;
 system("rm GRADIENTS ujo.out");
 isok = system("./A9OPTIM > ujo.out");
 Grad = load('GRADIENTS');
