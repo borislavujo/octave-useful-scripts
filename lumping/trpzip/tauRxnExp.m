3f2269c7316c6defeec6079205b02f08624a6f7d
Boris Fackovec <bf269@cygnus.ch.cam.ac.uk>
2015-05-30 12:28:54 +0100

    matlab-compatible version of the same

--- lumping/trpzip/tauRxnExp.m
+++ lumping/trpzip/tauRxnExp.m
@@ -1,68 +1,46 @@
-% difference from the others (g1,g4,g6)
-% sparse matrix is used instead of a full matrix
-
-% calculates relaxation time
-% K .. rate matrix
-% vp .. vector of equilibrium populations
-% vasi .. assignment into 2 states
-% vConn .. indices of states connected to the native state
 tic
 ratioEnd = 1e-5;
 startdt = 1e-1;
-%F = load('F');
-K = load('K').K;
-%K = K/max(max(K));
-%save K K
-maxTerm = max(max(K))
-minTerm = min(min(K))
-%K = full(K);
-n = size(K,1)
-% remove disconnected states
-vConn = load('vConn');
-K = K(vConn,:);
-K = K(:,vConn);
-[n,m] = size(K)
-vpe = load('vp');
-vpe = vpe(vConn);
-vpe = vpe/sum(vpe);
-% assignment to groups
-vas = load('vas');
-vasi = 2*ones(n,1);
-for i=1:n
-  if (ismember(vConn(i),vas))
-    vasi(i) = 1;
-  endif
-endfor
+WK = load('K','-ascii'); % K is a sparse rate matrix
+n = max(max(WK(:,1:2)))
+K = zeros(n) ; % full rate matrix
+for i=1:size(WK,1)
+    i1 = WK(i,1);
+    i2 = WK(i,2);
+    k = WK(i,3);
+    K(i1,i2) = k;
+end
+vpe = load('vp'); % equilibrium population
+vasi = load('vas'); % assignment into states
 
-% checks
-p1 = sum(vpe(find(vasi==1)))
+p1 = sum(vpe(find(vasi==1))) % equilibrium population of state 1
 p2 = sum(vpe(find(vasi==2)))
 p = 1; vp0 = vpe.*(vasi==1); vp0 = vp0 / p;
 
-% key variables for propagation
 dt0 = startdt/max(abs(diag(K))) % quite arbitrary compromise between efficiency and accuracy
-F = (eye(n)+dt0*K);
-t = 0;
-tau = 0;
-progress = 1-p
-beforePropagation = toc
+F = (eye(n)+dt0*K); % transition matrix
+t = 0; tau = 0; progress = 1-p
+beforePropagation = toc % time this
 while (1-progress>ratioEnd)
-  vp = F*vp0;
-  sparseness = nnz(F)/(n^2)
-  F = F*F;
-  vp = vp/sum(vp);
+  vp = F*vp0; % calculate population at time t
+  sparseness = nnz(F)/(n^2) % display transition matrix 'sparseness'
+  F = F*F; % t -> 2*t
+  vp = vp/sum(vp); % correct for numerical errors
+% fit to exponential
   pold = p;
   p = (sum(vp(find(vasi==1)))-p1)/(1-p1)
   a0 = pold^2/p
   k = log(pold/p)/t
-  tau += (pold-p)/(k*a0)
-  tauEstd = tau + p/k
+  tau = tau + (pold-p)/(k*a0) % integrate the segment
+  tauEstd = tau + p/k % tau estimated from current tau and k
   progress = 1-p
-  if (t==0) t=dt0 else t *= 2 endif
+  if (t==0) t=dt0; else t = t*2; end
   computerTime = toc
-endwhile
+end
 
 tauNow = tau
-dp = sum((K*vp)(find(vasi==1)))/(1-p1)
+vdp = K*vp;
+dp = sum(vdp(find(vasi==1)))/(1-p1)
 k = -dp/p
-tau = tau + p/k
+tau = tau + p/k % add the tail
+save -ascii tauFinal tau
