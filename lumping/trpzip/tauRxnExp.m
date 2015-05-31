7a02f6fb112b9f2d5dc8ed1e27451875d42deb9f
Boris Fackovec <bf269@sinister.cm.cluster>
2015-05-31 17:04:16 +0100

    refinement

--- lumping/trpzip/tauRxnExp.m
+++ lumping/trpzip/tauRxnExp.m
@@ -1,6 +1,7 @@
 tic
 ratioEnd = 1e-5;
 startdt = 1e-1;
+% load the rate matrix
 WK = load('K','-ascii'); % K is a sparse rate matrix
 n = max(max(WK(:,1:2)))
 K = zeros(n) ; % full rate matrix
@@ -11,36 +12,39 @@ for i=1:size(WK,1)
     K(i1,i2) = k;
 end
 vpe = load('vp'); % equilibrium population
-vasi = load('vas'); % assignment into states
-
-p1 = sum(vpe(find(vasi==1))) % equilibrium population of state 1
-p2 = sum(vpe(find(vasi==2)))
-p = 1; vp0 = vpe.*(vasi==1); vp0 = vp0 / p;
-
+Lump = load('Lump','-ascii'); % assignment into states
+nLumps = size(Lump,2);
+% set up the starting population
+Peq = repmat(vpe,1,nLumps);
+vp1 = sum(Peq.*Lump) % equilibrium population of state 1
+Pop0 = Peq.*Lump./repmat(vp1,n,1);
+vp = sum(Pop0);
+% the first transition matrix
 dt0 = startdt/max(abs(diag(K))) % quite arbitrary compromise between efficiency and accuracy
 F = (eye(n)+dt0*K); % transition matrix
-t = 0; tau = 0; progress = 1-p
-beforePropagation = toc % time this
-while (1-progress>ratioEnd)
-  vp = F*vp0; % calculate population at time t
-  sparseness = nnz(F)/(n^2) % display transition matrix 'sparseness'
+t = 0; vTau = 0; beforePropagation = toc % time this
+% relaxation
+while (max(abs(vp))>ratioEnd)
+  Pop = F*Pop0; % calculate population at time t
+  Pop = Pop ./ repmat(sum(Pop),n,1);
   F = F*F; % t -> 2*t
-  vp = vp/sum(vp); % correct for numerical errors
+  F(abs(F) < sqrt(realmin)) = 0;
+  sparseness = nnz(F)/(n^2) % display transition matrix 'sparseness'
 % fit to exponential
-  pold = p;
-  p = (sum(vp(find(vasi==1)))-p1)/(1-p1)
-  a0 = pold^2/p
-  k = log(pold/p)/t
-  tau = tau + (pold-p)/(k*a0) % integrate the segment
-  tauEstd = tau + p/k % tau estimated from current tau and k
-  progress = 1-p
+  vpold = vp;
+  p = sum(Lump.*Pop)./(ones(1,nLump)-vp1)
+  vTau = vTau + t*(vp*vpold)/2
+% estimate final tau
+  DP = K*Pop;
+  vdp = sum(DP.*Lump)./(ones(1,nLump)-vp1)
+  vk = -vdp./vp
+  vTauEstd = vTau + vp./vk % tau estimated from current tau and k
   if (t==0) t=dt0; else t = t*2; end
   computerTime = toc
 end
 
-tauNow = tau
-vdp = K*vp;
-dp = sum(vdp(find(vasi==1)))/(1-p1)
-k = -dp/p
-tau = tau + p/k % add the tail
-save -ascii tauFinal tau
+DP = K*Pop;
+vdp = sum(DP.*Lump)./(ones(1,nLump)-vp1)
+vk = -vdp./vp
+vTau = vTau + vp./vk % add the tail
+save -ascii vTau vTau
