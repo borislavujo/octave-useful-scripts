7644d4c41ea3311f7ef7ea744c0581a2d9a46657
Boris Fackovec <borislavujo@gmail.com>
2015-06-04 08:51:09 +0100

    Update test.m

--- kmc/test.m
+++ kmc/test.m
@@ -1,26 +1,17 @@
-%WK = load('K','-ascii'); % K is a sparse rate matrix
-%n = max(max(WK(:,1:2)))
-%K = zeros(n) ; % full rate matrix
-%for i=1:size(WK,1)
-%    i1 = WK(i,1);
-%    i2 = WK(i,2);
-%    k = WK(i,3);
-%    K(i1,i2) = k;
-%end
+nKMCSteps = 10000;
 K = load('K');
-
 [vN, IN, P] = rmat2kmc(K)
 ind = 1;
 indLast = ind;
-NTrans = zeros(4);
+NTrans = zeros(4); % record transitions between states 1..4
 tTot = 0;
-for i=1:100000
+for i=1:nKMCSteps
   indOld = ind;
-  [ind,t] = kmc1(ind, vN, IN, P, K)
-  tTot = tTot + t
+  [ind,t] = kmc1(ind, vN, IN, P, K);
+  tTot = tTot + t;
   if ismember(ind,[1:4])
-    NTrans(ind,indLast) = NTrans(indLast,ind) +1
+    NTrans(ind,indLast) = NTrans(indLast,ind) +1;
     indLast = ind;
-  endif
+  end
 end
 save NTrans NTrans
