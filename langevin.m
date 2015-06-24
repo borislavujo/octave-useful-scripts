a63ae571cd855c877c4ee691eaeb0e3617bc1b4d
Boris Fackovec <bf269@sinister.cm.cluster>
2015-06-24 11:07:55 +0100

    Langevin propagator for varying masses

--- langevin.m
+++ langevin.m
@@ -10,15 +10,15 @@ if (nargin<6) vm = ones(n,1); endif % assign default masses
 if (size(vm,1)~=n) error("number of masses not equal to number of particles"); endif
 
 % a, b ... auxiliary variables
-a = exp(-gamma*dt);
-if (gamma == 0) b = 1; else b = sqrt(tanh(gamma*dt/2)/(gamma*dt/2)); endif % limit b (gamma=0) = 1 
+va = exp(-gamma*dt*ones(n,1)./vm);
+if (gamma == 0) vb = ones(n,1); else vb = sqrt(tanh(0.5*gamma*dt*ones(n,1)./vm)./(0.5*gamma*dt*ones(n,1)./vm)); endif % limit b (gamma=0) = 1 
 
 % OVRVO integrator, equations 7a-g in arXiv 1301.3800
-Xt = sqrt(a)*Xt + sqrt(1-a)*normrnd(0,1,size(X,1),2)./sqrt(beta*repmat(vm,1,k)); % 7a 
+Xt = repmat(sqrt(va),1,k).*Xt + repmat(sqrt(ones(n,1)-va),1,k).*normrnd(0,1,n,k)./sqrt(beta*repmat(vm,1,k)); % 7a 
 [vEne,Xttm] = potential(X); % calculation of energy and energy gradient
-Xt = Xt - (dt/2)*b*Xttm./repmat(vm,1,k); % 7b (acceleration = - energy gradient / mass)
-X = X + (dt/2)*b*Xt; % 7c
+Xt = Xt - ((dt/2)*repmat(vb,1,k).*Xttm./repmat(vm,1,k)); % 7b (acceleration = - energy gradient / mass)
+X = X + (dt/2)*repmat(vb,1,k).*Xt; % 7c
 [vEne,Xttm] = potential(X); % 7d evaluate gradient at midpoint
-X = X + (dt/2)*b*Xt; % 7e
-Xt = Xt - (dt/2)*b*Xttm./repmat(vm,1,k); % 7f
-Xt = sqrt(a)*Xt + sqrt(1-a)*normrnd(0,1,size(X,1),2)./sqrt(beta*repmat(vm,1,k)); % 7g
+X = X + ((dt/2)*repmat(vb,1,k).*Xt); % 7e
+Xt = Xt - ((dt/2)*repmat(vb,1,k).*Xttm./repmat(vm,1,k)); % 7f
+Xt = repmat(sqrt(va),1,k).*Xt + sqrt(repmat(ones(n,1)-va,1,k)).*normrnd(0,1,n,k)./sqrt(beta*repmat(vm,1,k)); % 7g
