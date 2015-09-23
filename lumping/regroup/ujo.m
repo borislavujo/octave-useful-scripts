6ddcb0049bd53c96d5ceb4e98a72565fe39f8b35
borislavujo <research@fackovec.net>
2015-09-23 17:03:39 +0100

    avoid grouping observables

--- lumping/regroup/ujo.m
+++ lumping/regroup/ujo.m
@@ -1,10 +1,19 @@
 R = load('R').R;
+% avoid grouping observables
+va{1} = [1:5]'; va{2} = [401:5310]'; va{3} = [6:400]';
+for i1=1:2
+  for i2=i1+1:3
+    R(va{i1},va{i2}) *= 1e-30;
+    R(va{i2},va{i1}) *= 1e-30;
+  endfor
+endfor
 vp = load('vp').vp;
-[K,vi] = regroupfree(R,vp,1e-5);
-vj = unique(vi); vi0 = vi; nFin = size(vj,1)
+[K,vi] = regroupfree(R,vp,1e-30,1210);
+vj = unique(vi);
+vi0 = vi; nFin = size(vj,1)
 for i=1:size(vj,1)
   vi(vi0==vj(i))=i;
 endfor
-vi = vi'
+%vi = vi';
 save K K
 save vi vi
