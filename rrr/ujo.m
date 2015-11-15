452683e895b802aed5ada92486a087f7e4f7bc1a
borislavujo <research@fackovec.net>
2015-11-15 21:30:06 +0100

    more reasonable vlump

--- rrr/ujo.m
+++ rrr/ujo.m
@@ -1,5 +1,4 @@
-Kl = load('Kl').Kl
-vpl = load('vpl')
+Kl = load('Kl').Kl;
+vpl = load('vpl');
 vil = load('vlump');
-vil(find(vil==0))=2
-[vi,FEl,SSl,TSTl,NFl] = rrr(Kl,vpl,0,vil) % group into a 2-state model with arbitrary observables
+[vi,FEl,SSl,TSTl,NFl] = rrr(Kl,vpl,2,vil) % group into a 2-state model with arbitrary observables
