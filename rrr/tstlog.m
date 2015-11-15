2913597c9b16d6fda82be7de091f6c634d83283e
borislavujo <research@fackovec.net>
2015-11-15 20:51:08 +0100

    detailed balance satisfied

--- rrr/tstlog.m
+++ rrr/tstlog.m
@@ -2,6 +2,5 @@ function [kABl, kBAl] = tstlog(Kl,vpl)
 % calculates TST rates between states (1+2) and 3 
 % 3x3 log rate matrix on input
 kABl = logSumExp(Kl(1:2,3));
-%kBAl = logSumExp(Kl(3,1:2)'+vpl(1:2))-logSumExp(vpl(1:2));
 kBAl = kABl + vpl(3) - logSumExp(vpl(1:2));
 endfunction
