b7cdd3e6a075133041aed6a1fd5b0df1268d2a45
Boris Fackovec <borislavujo@gmail.com>
2016-02-07 18:03:53 +0000

    debugged some

--- rrr/analyt/rrr.m
+++ rrr/analyt/rrr.m
@@ -1,4 +1,4 @@
-function [vi,FEl,SSl,TSTl,NFl] = rrr(Kl,vpl,nDesiredStates,vObserv)
+function [vi,FEl,SSl,TSTl,NFl] = rrr(Kl,vpl,nDesiredStates,vObserv,maxThresh)
 % grouping minima stops when either a rate constant threshold or a number of states is reached
 % returns matrices without diagonal terms
 % grouping is determined by largest of the lowest rate constants calculated using no-flux BC
@@ -8,9 +8,12 @@ function [vi,FEl,SSl,TSTl,NFl] = rrr(Kl,vpl,nDesiredStates,vObserv)
 % vObserv ... determines the states that should not be grouped together, as they belong to different observables
   n = size(Kl,1); nStates = n; % the original number of states
   if (nargin<4) vObserv = ones(n,1); endif % if anything can be grouped, make vObserv vector
+  if (nargin<5) maxThresh = -99e9; endif
   vObsInd = unique(vObserv); nObserv = size(vObsInd,1); % how many observable states there are
   if (nDesiredStates<nObserv) nDesiredStates = nObserv; endif % number of states at the end is at least the number of observables
   vi = [1:n]'; % index vector, always of size n
+  rozdiel = max(max(Kl))+20
+  Kl(find(Kl)) -= rozdiel;
   Kl = Kl - diag(diag(Kl)); % delete all diagonal (negative) terms; Kl is now the decision matrix
   Kl = Kl -1e19*eye(n); % fill the diagonal, so it does not interfere with the searches for the largest rate constant
   SSl = Kl; % steady state matrix = the lower bound
@@ -35,43 +38,48 @@ function [vi,FEl,SSl,TSTl,NFl] = rrr(Kl,vpl,nDesiredStates,vObserv)
     endfor
   endif
   [irow,icol,know] = maxNZ(min(Kl,Kl'));
-  while (nStates>nDesiredStates)
+  while and(nStates>nDesiredStates,know>maxThresh)
     [Kl,vpln,vin]   = groupStates(Kl,vi,vpl,irow,icol,"nf"); % new decision matrix, assignment, and population vector
     FEl  = groupStates(FEl,vi,vpl,irow,icol,"fe"); % fast-equilibrium B.C. rates
     SSl  = groupStates(SSl,vi,vpl,irow,icol,"ss"); % steady state approx. rates
     TSTl = groupStates(TSTl,vi,vpl,irow,icol,"ts"); % TST rates
     NFl  = groupStates(NFl,vi,vpl,irow,icol,"nf"); % no-flux B.C. rates
     [irow,icol,know] = maxNZ(min(Kl,Kl'))
-    kTSl = TSTl(irow,icol)
-    kFEl = FEl(irow,icol)
-    kNFl = NFl(irow,icol)
-    kSSl = SSl(irow,icol)
+    kTSl = TSTl(irow,icol)+rozdiel
+    kFEl = FEl(irow,icol)+rozdiel
+    kNFl = NFl(irow,icol)+rozdiel
+    kSSl = SSl(irow,icol)+rozdiel
     vpl = vpln; vi = vin;
     nStates = size(Kl,1) % print progress
   endwhile
+  FEl += rozdiel;
+  NFl += rozdiel;
+  TSTl += rozdiel;
+  SSl += rozdiel;
 endfunction
 
 function [Kln,vpl,vi] = groupStates(Kl,vi,vpl,is1,is2,kType)
 % rate matrix reduction by 1 state
+  malo = -9e9;
   is12 = min(is1,is2); is2 = max(is1,is2); is1 = is12;
   R12 = Kl(is1,is2); R21 = Kl(is2,is1);
   Kln = Kl; % new matrix is created, so that the gradual modifications do not affect
-  Kl(is1,is2) = -1e9; Kl(is2,is1) = -1e9; Kl(is1,is1) = -1e9; Kl(is2,is2) = -1e9; % delete rates between s1 and s2, so they do not appear among neighbours
+  Kl(is1,is2) = malo; Kl(is2,is1) = malo; Kl(is1,is1) = malo; Kl(is2,is2) = malo; % delete rates between s1 and s2, so they do not appear among neighbours
 %  Kl(is1,find(Kl(is1,:)<-1e5))=0; Kl(is2,find(Kl(is2,:)<-1e5))=0; Kl(find(Kl(:,is1)<-1e5),is1)=0; Kl(find(Kl(:,is2)<-1e5),is1)=0; % discard too small rates
-  vnei = unique([find(Kl(is1,:)>-1e8),find(Kl(is2,:)>-1e8)]); % find all the neighbours of s1 and s2
+  vnei = unique([intersect(find(Kl(is1,:)>-1e8),find(Kl(is1,:))),intersect(find(Kl(is2,:)>-1e8),find(Kl(is2,:)))]); % find all the neighbours of s1 and s2
   [nnj,nj] = size(vnei)
   if and(nj>0,nnj>0)
     for j=vnei
       vpj = [vpl(is1);vpl(is2);vpl(j)]; vpj = vpj - logSumExp(vpj); % vector of 3 eq. popul.
-      Kij = [0,R12,Kl(is1,j);R21,0,Kl(is2,j);Kl(j,is1),Kl(j,is2),0]; % 3x3 log rate matrix
+      Kij = [malo,R12,Kl(is1,j);R21,malo,Kl(is2,j);Kl(j,is1),Kl(j,is2),malo]; % 3x3 log rate matrix
 %      if (max(max(Kij))-min(min(Kij))>1e3)
 %	davame = kType
 %	betweenObserv = [is1, is2, j];
 %	[kab,kba] = tstlog(Kij,vpj);
       if (prod(kType=="fe")==1)
 	davame = [kType, "fe"]
-	vKl1 = Kl(:,is1); vKl1(is2) = 0; vKl1(j) = 0; vKl1 = full(vKl1(find(vKl1>-1e8))); kADl = logSumExp(vKl1); % adding the equilibration through the neighbours
-	vKl2 = Kl(:,is2); vKl2(is1) = 0; vKl2(j) = 0; vKl2 = full(vKl2(find(vKl2>-1e8))); kBDl = logSumExp(vKl2);
+	vKl1 = Kl(:,is1); vKl1(is2) = malo; vKl1(j) = malo; vKl1 = full(vKl1(find(vKl1>-1e8))); kADl = logSumExp(vKl1); % adding the equilibration through the neighbours
+	vKl2 = Kl(:,is2); vKl2(is1) = malo; vKl2(j) = malo; vKl2 = full(vKl2(find(vKl2>-1e8))); kBDl = logSumExp(vKl2);
 	sumflux = logSumExp([vpl(1)+kADl;vpl(2)+kBDl]); 
 	f1l = vpl(1)+kADl-sumflux; 
 	f2l = vpl(2)+kBDl-sumflux; 
@@ -87,6 +95,12 @@ function [Kln,vpl,vi] = groupStates(Kl,vi,vpl,is1,is2,kType)
       else
 	vdavame = kType
 	[kab,kba] = tstlog(Kij,vpj);
+      endif
+      if (kab>1e3)
+	UkazKij = Kij
+	vukazpl = vpj
+	ukazkab = kab
+	ukazkba = kba
       endif
       Kln(is1,j) = kab;
       Kln(j,is1) = kba;
