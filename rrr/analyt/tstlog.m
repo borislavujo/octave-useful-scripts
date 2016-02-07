e7b006e019553c009e21f842c73e50dbe0016db0
Boris Fackovec <borislavujo@gmail.com>
2016-02-07 18:04:56 +0000

    some more changes

--- rrr/analyt/tstlog.m
+++ rrr/analyt/tstlog.m
@@ -2,6 +2,6 @@ function [kABl, kBAl] = tstlog(Kl,vpl)
 % calculates TST rates between states (1+2) and 3 
 % 3x3 log rate matrix on input
 vk = Kl(1:2,3);
-kABl = logSumExp(vk(find(vk)));
-kBAl = kABl + vpl(3) - logSumExp(vpl(1:2));
+kABl = logSumExp(vk(find(vk)))
+kBAl = kABl + vpl(3) - logSumExp(vpl(1:2))
 endfunction
